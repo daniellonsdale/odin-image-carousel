36c171100ca9638ed7f5d12efa30d8c98de229be
daniellonsdale <danielllonsdale@gmail.com>
2025-03-02 17:39:05 +0900

    Add needed element consts and picture state tracker

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import "./styles.css";
+
+const leftArrow = document.querySelector('.left-arrow');
+const rightArrow = document.querySelector('.right-arrow');
+const dotOne = document.querySelector('.dot-1');
+const dotTwo = document.querySelector('.dot-2');
+const dotThree = document.querySelector('.dot-3');
+const dotFour = document.querySelector('.dot-4');
+const dotFive = document.querySelector('.dot-5');
+const pictureHolder = document.querySelector('.picture-holder');
+let curPicture = 1;
